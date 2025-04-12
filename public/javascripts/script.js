bc3c77c8686033bff3b51ebbdefcaea7ef1d9acb
totallyallen <86538101+totallyallen@users.noreply.github.com>
2025-04-12 16:55:00 -0400

    Fixed scaling problems, added actual instructions, tweaked appearance of results page a bit.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,7 +3,21 @@ let return_btn = document.getElementById('instructions-cancel')
 
 let btns = document.getElementsByClassName('btn');
 
-const INSTRUCTIONS_TEXT = "Welcome to our small project Quizzd! We hope you enjoy this demo.";
+const INSTRUCTIONS_TEXT = `Welcome to our small project:&nbsp;
+
+                            <span style="letter-spacing: -8px; text-shadow: #03ffff 1px 2px,#ff00ff -1px -3px; font-family: 'Press Start'">
+                                <span style="color: red;">Q</span>
+                                <span style="color: orange; font-size: .75em; margin-bottom: -35px;"><sub>U</sub></span>
+                                <span style="color: yellow; font-size: .75em;">I</span>
+                                <span style="color: green; font-size: .75em; margin-top: -50px;"><sup>Z</sup></span>
+                                <span style="color: blue; font-size: .75em; margin-bottom: -35px;"><sub>Z</sub></span>
+                                <span style="color: #8c52ff;">D</span>
+                            </span><br><br>
+
+                            Given a question and 4 choices, 
+                            you have to correctly answer as many questions as possible before time is up.<br><br>
+                            
+                            Do you have what it takes?`;
 document.getElementById('instructions-text').innerHTML = INSTRUCTIONS_TEXT;
 
 // Prevent spamming when button animation is underway
