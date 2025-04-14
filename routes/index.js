ad203829a3eae5007ecb019cb4bae3718e08812d
Snomysnow <187812257+Snomysnow@users.noreply.github.com>
2025-04-14 19:21:47 -0400

    added score with total questions to results

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,11 @@ router.get('/', function(req, res, next) {
     res.render('./home/index', { title : 'Quizzd'});
 })
 
-router.get('/results', function(req, res, next) {
-    res.render('./main/results', { title : 'Results', score: '4'});
+router.get('/results', (req, res) => {
+    res.render('./main/results', {
+        score: req.session.score,
+        totalQuestions: req.session.totalQuestions
+    });
 });
 
 module.exports = router;
