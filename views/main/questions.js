cc0bf6b79c7ab6d49e1dbcca7293edd367df935b
Shaine Lomenario <shainelomenario@gmail.com>
2025-04-13 23:45:21 -0400

    Made a user session

--- views/main/questions.js
+++ views/main/questions.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
+
+
+router.post('/', (req, res) => {
     const numOfQuestions = req.body["questions"];
+
+    // Current user session
+    req.session.questions = getRandomQuestions();
+    req.session.currentQuestion = 0;
+    req.session.score = 0;
+
+
 });
 
 function loadQuestions() {
@@ -16,16 +26,15 @@ function loadQuestions() {
     }
 }
 
-function getRandomQuestions() {
-    
+function getRandomQuestions(count = numOfQuestions) {
     const shuffled = [...questionsData].sort(() => 0.5 - Math.random());
     return shuffled.slice(0, count)
 }
 
-function checkAnswer(questionIndex, userAnswer) {
-    const question = questionsData[questionIndex];
-    if (!question) return false;
-    return question.answer === userAnswer;
+function checkAnswer(questionIndex, userAnswer, questions) {
+    const currQuestion = questions[questionIndex];
+    if (!currQuestion) return false;
+    return currQuestion.answer === userAnswer;
 }
 
 function getOptionsForQuestion(question) {
